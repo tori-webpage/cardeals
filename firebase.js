9b6ff97d867943ca26d64d497ac0cebd0ac8e7b5
tori-webpage <cheapways91@gmail.com>
2026-06-08 20:26:48 +0200

    Delete firebase.js

--- firebase.js
+++
@@ -1,3 +0,0 @@
-const firebaseConfig={apiKey:"AIzaSyAU8hVTqury1MbC0Sx-CXGY1YQrr5VJhyE",authDomain:"cardeals-al.firebaseapp.com",projectId:"cardeals-al",storageBucket:"cardeals-al.firebasestorage.app",messagingSenderId:"839953227675",appId:"1:839953227675:web:6fb6aa9350bd3bd507cb3e"};
-firebase.initializeApp(firebaseConfig);
-const db=firebase.firestore(),auth=firebase.auth(),PLATFORM_FEE=0.08;
